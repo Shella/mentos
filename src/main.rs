6c699a2db12e453e48a4350570653e8ef72a7782
Shella Stephens <pirateshella@gmail.com>
2018-02-11 19:19:13 -0800

    smaller fn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate env_logger;
 extern crate failure;
-#[macro_use]
-extern crate serde_derive;
+#[macro_use] extern crate serde_derive;
 extern crate ssh2;
+#[macro_use] extern crate structopt;
 extern crate toml;
 use failure::Error;
-use ssh2::Session as SessionClient;
+use ssh2::Session;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
@@ -17,42 +17,55 @@ fn main() {
     let device_config = read_toml().unwrap();
     let (_name, device) = device_config.devices.into_iter().next().unwrap();
     println!("RouterConfig: {:#?}", device);
-    let ip = device.ip.unwrap();
-    let port = device.port.unwrap();
-    let user = device.user.unwrap();
-    let os = device.os.unwrap();
 
-    let sess = SessionClient::new().unwrap();
-    let mut agent = sess.agent().unwrap();
-    agent.connect().unwrap();
-    agent.list_identities().unwrap();
-
-    for identity in agent.identities() {
-        let identity = identity.unwrap();
-        println!("SSH Agent Identity: {}", identity.comment());
-        let pubkey = identity.blob();
-        println!("Key: {:?}", pubkey);
-    }
-
+    let ip = device.ip.as_ref().unwrap();
+    let port = device.port.as_ref().unwrap();
+    let user = device.user.as_ref().unwrap();
     let server = format!("{}:{}", ip, port);
+
+    let mut session = ssh2_session();
     println!("Connecting to server {}..", server);
     let tcp = TcpStream::connect(server).unwrap();
-    let mut sess = SessionClient::new().unwrap();
-    sess.handshake(&tcp).unwrap();
-    sess.userauth_agent(&user).unwrap();
-    assert!(sess.authenticated());
-    let banner = sess.banner().unwrap();
+    session.handshake(&tcp).unwrap();
+    session.userauth_agent(user).unwrap();
+
+    assert!(session.authenticated());
+    let banner = session.banner().unwrap();
     println!("Server Banner: {}", banner);
 
-    if os == "Junos OS" {
-        let mut channel = sess.channel_session().unwrap();
-        channel.exec("show configuration").unwrap();
-        let mut s = String::new();
-        channel.read_to_string(&mut s).unwrap();
-        println!("{}", s);
-        channel.wait_close();
+    if device.os.as_ref().unwrap() == "Junos OS" {
+        let channel = junos_cmd_show_configuration(&session)
+            .expect("Unable to open channel");
         println!("{}", channel.exit_status().unwrap());
     }
+}
+
+fn ssh2_session() -> ssh2::Session {
+    let sess = Session::new().unwrap();
+    {
+        let mut agent = sess.agent().unwrap();
+        agent.connect().unwrap();
+        agent.list_identities().unwrap();
+
+        for identity in agent.identities() {
+            let identity = identity.unwrap();
+            println!("SSH Agent Identity: {}", identity.comment());
+            let pubkey = identity.blob();
+            println!("Key: {:?}", pubkey);
+        }
+    }
+
+    return sess;
+}
+
+fn junos_cmd_show_configuration(session: &Session) -> Result<ssh2::Channel, Error> {
+    let mut channel = session.channel_session().unwrap();
+    channel.exec("show configuration").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s)?;
+    println!("{}", s);
+
+    Ok(channel)
 }
 
 #[derive(Debug, Deserialize)]
@@ -76,4 +89,15 @@ fn read_toml() -> Result<Config, Error> {
     f.read_to_string(&mut contents)?;
 
     Ok(toml::from_str::<Config>(&contents.to_string())?)
+}
+
+#[derive(StructOpt)]
+#[structopt(name = "mentos", about = "the fresh maker")]
+enum Mentos {
+    #[structopt(name = "fetch")]
+    Fetch {
+        #[structopt(short = "all")]
+        all: bool,
+        input: Option<String>
+    },
 }
